db88e3c09bc705ee7b3bb1f00c56909ba1dcebdd
Lauren Birts <birtslauren@gmail.com>
2020-03-31 04:46:45 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -18,7 +18,21 @@ def consolidate_cart(cart)
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
-
+   item_index = 0
+  unique_array = []
+  while cart[item_index] do
+    item = cart[item_index]
+    item_hash = find_item_by_name_in_collection(item[:item], unique_array)
+    if item_hash
+      item_hash[:count] += 1
+    else 
+      item_hash = item
+      item_hash[:count] = 1
+      unique_array << item_hash
+    end
+    item_index += 1
+  end
+  unique_array
 end
 
 
