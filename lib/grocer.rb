d0202d25caff09970b133d8f98b203f29ff2dd1e
Lauren Birts <birtslauren@gmail.com>
2020-03-31 04:46:19 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -2,7 +2,15 @@ def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
-
+  item_index = 0
+  while collection[item_index] do
+    item = collection[item_index]
+  if name == item[:item]
+    return item
+  end
+  item_index += 1
+end
+nil
 end
 
 def consolidate_cart(cart)
